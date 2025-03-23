f1d7c7b148064caee8cb3e22ac953d8053b58644
NgThanhNhanf <nguyenthanhnhan9a$.19.20.nts@gmail.com>
2025-03-23 19:09:49 +0700

    finished header

--- src/main/java/org/projects/GUI/Components/header/headerBar.java
+++ src/main/java/org/projects/GUI/Components/header/headerBar.java
@@ -1,16 +1,21 @@
 package org.projects.GUI.Components.header;
 
+import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class headerBar extends javax.swing.JPanel {
     public headerBar(String listItemHeader[][]) {
-        this.setPreferredSize(new Dimension(1100,100));
+//        this.setBorder(new RoundedBorder(8));
+        this.setPreferredSize(new Dimension(950,100));
+        this.setLayout(new FlowLayout(FlowLayout.LEFT,5, 0));
+        this.setOpaque(true);
+        this.setBackground(Color.WHITE);
         init(listItemHeader);
         this.setVisible(true);
     }
     public void init(String listItemHeader[][]) {
-        this.setLayout(new FlowLayout(FlowLayout.LEFT,0, 0));
-        this.add(new headerFunction(new Dimension(1100, 100), listItemHeader));
-        this.add(new headerSearch(new Dimension(1100, 100)));
+        this.add(new headerSearch(new Dimension(950, 100)));
+        this.add(new headerFunction(new Dimension(950, 100), listItemHeader));
     }
 }
