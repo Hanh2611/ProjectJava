ee2422ce1867a8415ede14d95134dbc85ae65dfa
qbael <baohoo10205@gmail.com>
2025-03-26 21:11:01 +0700

    push product

--- src/main/java/org/projects/GUI/Panel/SanPham.java
+++ src/main/java/org/projects/GUI/Panel/SanPham.java
@@ -1,8 +1,10 @@
 package org.projects.GUI.Panel;
 
-import java.awt.Color;
+import java.awt.*;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 
 public class SanPham extends JPanel{
 
@@ -15,8 +17,30 @@ public class SanPham extends JPanel{
     }
 
     private void initComponent() {
+
         String[] columns = {"Hình ảnh", "Tên", "Phân loại", "Giá bán", "Số lượng tồn", "Quy cách", "Đơn vị"};
-        String[][] data = {};
-        table = new JTable();
+        Object[][] data ={
+                {"src/main/resources/img/product/tra-xanh-c2.jpg", "Trà xanh C2", "Nước giải khát", 10000, "100", "Chai", "330ml"},
+                {"src/main/resources/img/product/suon-bo-my.jpg", "Sườn bò Mỹ", "Thịt", 200000, "50", "Hộp", "300g"}
+        };
+
+        for (Object[] it : data){
+            it[0] = new ImageIcon(new ImageIcon(it[0].toString())
+                    .getImage().getScaledInstance(80, 60, Image.SCALE_SMOOTH));
+        }
+
+        DefaultTableModel model = new DefaultTableModel(data, columns) {
+            @Override
+            public Class<?> getColumnClass(int column) {
+                return (column == 0) ? ImageIcon.class : Object.class;
+            }
+        };
+
+        JTable table = new JTable(model);
+        table.setRowHeight(60);
+        table.setDefaultEditor(Object.class, null);
+        table.setPreferredScrollableViewportSize(new Dimension(950, 650));
+        table.setFillsViewportHeight(true);
+        this.add(new JScrollPane(table));
     }
 }
