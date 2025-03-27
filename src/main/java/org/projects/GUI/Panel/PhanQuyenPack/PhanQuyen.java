6e4f5fc2d63f988c48080156d7dd1b2b2971be71
NgThanhNhanf <nguyenthanhnhan9a$.19.20.nts@gmail.com>
2025-03-27 16:46:04 +0700

    code phan quyen

--- src/main/java/org/projects/GUI/Panel/PhanQuyenPack/PhanQuyen.java
+++ src/main/java/org/projects/GUI/Panel/PhanQuyenPack/PhanQuyen.java
@@ -2,14 +2,14 @@ package org.projects.GUI.Panel.PhanQuyenPack;
 
 import org.projects.GUI.Components.header.headerBar;
 import org.projects.GUI.Components.layoutCompoment;
-
 import java.awt.*;
-
 import javax.swing.*;
 import javax.swing.border.CompoundBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class PhanQuyen extends JPanel{
     private JTable mainTable;
+    private JPanel contentPanel;
     public PhanQuyen() {
         String listItemHeader[][] = {
                 {"icon/add.svg", "Thêm", "add"},
@@ -20,11 +20,24 @@ public class PhanQuyen extends JPanel{
         };
         layoutCompoment.addHeader(this, listItemHeader);
         init();
+        initTable();
     }
     public void init() {
-
+        contentPanel = new JPanel();
+        contentPanel.setLayout(new BorderLayout());
+        layoutCompoment.setupMainPanel(contentPanel);
+        this.add(contentPanel);
     }
     public void initTable() {
-
+        String [] col = {"Mã nhóm quyền", "Tên nhóm quyền"};
+        DefaultTableModel tableModel = new DefaultTableModel(col, 0) {
+            public boolean isCellEditable(int row, int column) {return false;}
+        };
+        tableModel.addRow(new Object[] {"111", "Bố"});
+        mainTable = new JTable(tableModel);
+        for (int i = 0; i < mainTable.getColumnCount(); i++) {
+            mainTable.getColumnModel().getColumn(i).setPreferredWidth(200);
+        }
+        contentPanel.add(mainTable, BorderLayout.CENTER);
     }
 }
--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,1 +1,7 @@
 show databases;
+use quanlysieuthimini;
+INSERT INTO nhom_quyen (ma_nhom_quyen, ten_nhom_quyen) VALUES
+                                                      (1, 'Quản trị viên'),
+                                                      (2, 'Nhân viên'),
+                                                      (4, 'Quản lý'),
+                                                      (3, 'Khách');
