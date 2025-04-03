e0dabd2d51a37fd0e569c13aecc209a37b0d8986
NHanh <nha261105@gmail.com>
2025-04-03 15:36:42 +0700

    update nut

--- src/main/java/org/projects/GUI/Components/header/headerFunction.java
+++ src/main/java/org/projects/GUI/Components/header/headerFunction.java
@@ -12,28 +12,50 @@ import java.awt.event.MouseEvent;
 import java.util.HashMap;
 import java.util.Map;
 
-public class headerFunction extends JPanel {
+public class headerFunction extends JToolBar {
     private HashMap<String,generalFunction> hm = new HashMap<>();
-    public headerFunction(Dimension parentSize, String listItemHeader[][]) {
+    public headerFunction(Dimension parentSize, String listItemHeader[][],String[] listAction) {
         this.setPreferredSize(new Dimension((int) (390), parentSize.height));
-        this.setLayout(new GridBagLayout());
+        this.setFloatable(false);
+//        this.setLayout(new GridBagLayout());
         this.setBackground(Color.WHITE);
-        init(listItemHeader);
+        init(listItemHeader,listAction);
         this.setVisible(true);
     }
-    public void init(String listItemHeader[][]) {
-        GridBagConstraints c = new GridBagConstraints();
-        c.gridx = 0;
-        c.gridy = 0;
-        c.weightx = 0.5;
-        c.weighty = 0.5;
-        c.anchor = GridBagConstraints.CENTER;
-        for (String [] i : listItemHeader) {
-          generalFunction  gl = new generalFunction(i[0],i[1],i[2]);
-//            hm.put(i[2],gl);
-            this.add(gl, c);
-            c.gridx++;
+    public void init(String listItemHeader[][],String[] listAction) {
+//        GridBagConstraints c = new GridBagConstraints();
+//        c.gridx = 0;
+//        c.gridy = 0;
+//        c.weightx = 0.5;
+//        c.weighty = 0.5;
+//        c.anchor = GridBagConstraints.CENTER;
+        for(String la : listAction) {
+            switch (la) {
+                case "add":
+                    hm.put("add",new generalFunction("icon/add.svg","Thêm","add"));
+                    break;
+                case "update":
+                    hm.put("update",new generalFunction("icon/content-writing.svg","Sửa","update"));
+                    break;
+                case "delete":
+                    hm.put("delete",new generalFunction("icon/trash.svg","Xóa","delete"));
+                    break;
+                case "detail":
+                    hm.put("detail",new generalFunction("icon/details.svg","Chi tiết","detail"));
+                    break;
+            }
         }
+
+        for(generalFunction gf : hm.values()) {
+            this.add(gf);
+        }
+
+//        for (String [] i : listItemHeader) {
+//          generalFunction  gl = new generalFunction(i[0],i[1],i[2]);
+//            this.add(gl, c);
+//            this.add(gl);
+//            c.gridx++;
+//        }
     }
     //getter
     public HashMap<String,generalFunction> getHm() {
