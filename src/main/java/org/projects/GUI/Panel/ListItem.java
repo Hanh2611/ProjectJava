02de19ca5f9580d45b910b3f7867f41074c00775
Thaideptrai2810 <hongthaipham62@gmail.com>
2025-04-08 15:30:52 +0700

    list

--- src/main/java/org/projects/GUI/Panel/ListItem.java
+++ src/main/java/org/projects/GUI/Panel/ListItem.java
@@ -1,24 +1,21 @@
 package org.projects.GUI.Panel;
 
+import org.projects.BUS.PhanQuyenBUS;
+import org.projects.GUI.Components.MenuItemComponents;
+import org.projects.GUI.LoginGUI;
+import org.projects.GUI.MainGUI;
+import org.projects.GUI.Panel.NhanVienPack.NhanVien;
+import org.projects.GUI.Panel.PhanQuyenPack.PhanQuyen;
+import org.projects.GUI.Panel.ThongkePack.ThongKe;
+import org.projects.GUI.utils.Session;
+
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
-import javax.swing.BoxLayout;
-import javax.swing.JPanel;
-
-
-import org.projects.BUS.PhanQuyenBUS;
-import org.projects.GUI.LoginGUI;
-import org.projects.GUI.MainGUI;
-import org.projects.GUI.Components.MenuItemComponents;
-import org.projects.GUI.Panel.NhanVienPack.NhanVien;
-import org.projects.GUI.Panel.PhanQuyenPack.PhanQuyen;
-import org.projects.GUI.Panel.ThongkePack.ThongKe;
-import org.projects.GUI.utils.Session;
 
 
 public class ListItem extends JPanel{
