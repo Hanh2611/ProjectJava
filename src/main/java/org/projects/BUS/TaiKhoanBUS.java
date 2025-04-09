5fcf3da7c8b4357d95bae68fd59cb324a3573707
NgThanhNhanf <nguyenthanhnhan9a$.19.20.nts@gmail.com>
2025-04-09 15:07:12 +0700

    tai khoan

---
+++ src/main/java/org/projects/BUS/TaiKhoanBUS.java
@@ -0,0 +1,16 @@
+package org.projects.BUS;
+
+import org.projects.DAO.TaiKhoanDAO;
+import org.projects.entity.TaiKhoan;
+
+import java.util.List;
+
+public class TaiKhoanBUS {
+    public static List<TaiKhoan> getListTaiKhoan() {
+        return new TaiKhoanDAO().showlist();
+    }
+
+    public static String getTenNguoiDung(int maNguoiDung) {
+        return new TaiKhoanDAO().getTenNguoiDung(maNguoiDung);
+    }
+}
--- src/main/java/org/projects/GUI/Panel/TaiKhoan.java
+++ src/main/java/org/projects/GUI/Panel/TaiKhoan.java
@@ -1,6 +1,7 @@
 package org.projects.GUI.Panel;
 
 import org.projects.BUS.PhanQuyenBUS;
+import org.projects.BUS.TaiKhoanBUS;
 import org.projects.GUI.Components.header.headerBar;
 import org.projects.GUI.Components.layoutCompoment;
 import org.projects.GUI.utils.Session;
@@ -8,8 +9,16 @@ import org.projects.GUI.utils.Session;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.List;
 
 //todo: mình sẽ cho tạo tài khoản mặc định sẽ là khách hàng, sau đó mình sẽ set tài khoản vào với nhân viên
+//todo: mã người dùng sẽ được tạo tự động rồi lấy cái mã đó để có thể set cho nhân viên
+//todo: trong phần chi tiết ta có tên người dùng(Lấy từ mã người dùng), tên đăng nhập, loại tài khoản, các quyền, trạng thái;
+//todo: (Nhớ dùng regex) trong phần thêm ta sẽ nhập tên đăng nhập, mật khẩu, mã nhân viên (vì trước đó cần có nhân viên nên ta sẽ tạo 1 người dùng, rồi sau đó tạo tài khoản với mã người dùng đó)
+//todo: (Nhớ dùng regex) trong phần sửa thì ta sẽ được quyền sửa mật khẩu, mã người dùng, quyền, trạng thái
+//todo: Trong phần quyền mình có thể dùng combobox kết hợp với tìm kiếm để có thể chọn nhóm quyền 1 cách dễ dàng
+//todo: Phần trạng thái cũng sẽ là combobox
+//todo: Xóa thì có thể xóa tài khoản, sau đó xóa người dùng
 public class TaiKhoan extends JPanel{
     private headerBar header;
     private JTable mainTable;
@@ -41,6 +50,16 @@ public class TaiKhoan extends JPanel{
     }
 
     public void initTable() {
+        String[] col = {"Tên người dùng", "Mã người dùng", "Tên đăng nhập", "Loại người dùng", "Trạng thái"};
+        tableModel = new DefaultTableModel(col, 0) {
+            public boolean isCellEditable(int row, int column) {return false;}
+        };
 
+    }
+    public void loadDataIntoTable() {
+        List<org.projects.entity.TaiKhoan> listTaiKhoan = TaiKhoanBUS.getListTaiKhoan();
+        for (org.projects.entity.TaiKhoan t : listTaiKhoan) {
+            tableModel.addRow(new Object[]{TaiKhoanBUS.getTenNguoiDung(t.getMaNguoiDung()), t.getMaNguoiDung(), t.getTenDangNhap(), "", t.getTrangThai()});
+        }
     }
 }
