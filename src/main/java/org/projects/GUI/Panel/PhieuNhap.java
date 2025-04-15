40a246c456c0a3aa4c39c6fc61ac0c0e0fa9edba
NHanh <nha261105@gmail.com>
2025-04-15 21:15:19 +0700

    cc

--- src/main/java/org/projects/GUI/Panel/PhieuNhap.java
+++ src/main/java/org/projects/GUI/Panel/PhieuNhap.java
@@ -1,12 +1,14 @@
 package org.projects.GUI.Panel;
 
 import org.projects.Action.PhieuNhapAction;
+import org.projects.BUS.PhanQuyenBUS;
 import org.projects.BUS.PhieuNhapBUS;
 import org.projects.DAO.PhieuNhapDAO;
 import org.projects.GUI.Components.header.headerBar;
 import org.projects.GUI.DiaLog.PhieuNhap.CapNhatPN;
 import org.projects.GUI.DiaLog.PhieuNhap.ChiTietPN;
 import org.projects.GUI.DiaLog.PhieuNhap.ThemPN;
+import org.projects.GUI.utils.Session;
 import org.projects.GUI.utils.UIUtils;
 import org.projects.entity.ChiTietPhieuNhapFullEntity;
 import org.projects.entity.PhieuNhapEntity;
@@ -41,7 +43,7 @@ public class PhieuNhap extends JPanel {
                 {"icon/trash.svg", "Xóa", "delete"},
                 {"icon/details.svg", "Chi tiết", "detail"}
         };
-        header = new headerBar(listItemHeader, new ArrayList<>(Arrays.asList("add", "update", "delete", "detail")), new String[]{"---", "mã", "tên", "địa chỉ"});
+        header = new headerBar(listItemHeader, Session.quyenTaiKhoan.get(PhanQuyenBUS.getMaDanhMuc("PhanQuyen") - 1),new String[]{"---","mã","tên","địa chỉ"});
         this.add(header);
         init();
         reloadDAO();
